9f4dd7c40fd5a31165759cef4a9c0ec2091e93d0
Bilal Alac <bilalalac@gmail.com>
2026-03-12 20:20:54 +0100

    Remove authentication URLs from kanmind_backend URL configuration

--- auth_user/api/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from auth_user.api.views import LoginView, UserProfileList, RegistrationView, UserProfileDetail, EmailCheckView
-
-urlpatterns = [
-]
--- kanmind_backend/urls.py
+++ kanmind_backend/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('auth_user.api.urls')),
     path('api/', include('kanmind_app.api.urls')),
 ]
